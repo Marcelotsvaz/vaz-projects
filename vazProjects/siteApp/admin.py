bcc44ece098d87e612c285d66b509e1159780c70
Marcelo Tellier Sartori Vaz <marcelotsvaz@gmail.com>
2021-08-23 10:05:33 -0300

    Change admin site title

--- vazProjects/siteApp/admin.py
+++ vazProjects/siteApp/admin.py
@@ -7,3 +7,10 @@
 
 
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+
+
+
+admin.site.site_title = _('VAZ Projects Admin Site')
+admin.site.site_header = _('Administration')
+admin.site.index_title = _('Home')
