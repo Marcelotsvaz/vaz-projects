3df9959ee49618e0683e9f8bb8a35ffb298e5921
Marcelo Tellier Sartori Vaz <marcelotsvaz@gmail.com>
2021-09-23 19:24:20 -0300

    Fix permissions for deployment folder duo to 81ed116

--- server/scripts/perInstance.sh
+++ server/scripts/perInstance.sh
@@ -23,16 +23,18 @@ hostnamectl set-hostname ${hostname}
 
 echo ${sshKey} > ~marcelotsvaz/.ssh/authorized_keys
 
-useradd -rm -s /usr/bin/nologin ${user}
+useradd -rms /usr/bin/nologin ${user}
 
 echo '::1 memcached' >> /etc/hosts	# TODO: Remove this.
 
-cd /home/${user}/
+cd ~${user}/
 
-aws s3 cp s3://${bucket}/${environment}/source.tar.gz - | sudo -u ${user} tar -xz
-sudo -u ${user} aws s3 sync s3://${bucket}/${environment}/deployment/ deployment/ --no-progress
+sudo -Eu ${user} bash << EOF
+aws s3 cp s3://${bucket}/${environment}/source.tar.gz - | tar -xz
+aws s3 sync s3://${bucket}/${environment}/deployment/ deployment/ --no-progress
 
-sudo -u ${user} mkdir deployment/{logs,static}/
+mkdir deployment/{logs,static}/
+EOF
 
 systemctl enable /home/${user}/server/systemdUnits/*
 
@@ -40,8 +42,8 @@ systemctl enable /home/${user}/server/systemdUnits/*
 
 # Django Setup
 ########################################
-sudo -EHu ${user} bash << 'EOF'
-python -m venv deployment/env
+sudo -Eu ${user} bash << EOF
+python -m venv deployment/env/
 source deployment/env/bin/activate
 
 pip install pip wheel --upgrade
