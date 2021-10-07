d5448ae279daf09169eddb1438dce0f7f1f45ab5
Marcelo Tellier Sartori Vaz <marcelotsvaz@gmail.com>
2021-10-06 22:46:18 -0300

    Implement break for blog post

--- vazProjects/commonApp/settings.py
+++ vazProjects/commonApp/settings.py
@@ -11,9 +11,20 @@ from django.conf import settings
 
 
 MARKDOWN_FEATURES = getattr( settings, 'MARKDOWN_FEATURES', [
+	# Block.
 	'heading',
+	'html_block',
 	'list',
+	
+	# Inline.
 	'emphasis',
-	'newline',
+	'html_inline',
+	'image',
 	'link',
+	'newline',
 ])
+
+MARKDOWN_OPTIONS = getattr( settings, 'MARKDOWN_FEATURES', {
+	'breaks': True,
+	'html': True,
+})
