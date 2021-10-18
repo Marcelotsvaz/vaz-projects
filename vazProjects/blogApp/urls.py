bafcff1973ae2edda27ac6b6efe68f17fcb07d03
Marcelo Tellier Sartori Vaz <marcelotsvaz@gmail.com>
2021-10-18 09:36:44 -0300

    Implement filtering posts by tags

--- vazProjects/blogApp/urls.py
+++ vazProjects/blogApp/urls.py
@@ -15,7 +15,9 @@ from . import views
 app_name = 'blogApp'
 
 urlpatterns = [
-	path( 'blog',					views.Blog.as_view(),	name = 'blog' ),
-	path( 'blog/page/<int:page>',	views.Blog.as_view(),	name = 'blog' ),
-	path( 'blog/<slug:slug>',		views.Post.as_view(),	name = 'post' ),
+	path( 'blog',										views.Blog.as_view(),		name = 'blog' ),
+	path( 'blog/page/<int:page>',						views.Blog.as_view(),		name = 'blog' ),
+	path( 'blog/tags/<slug:tag_slug>',					views.BlogByTag.as_view(),	name = 'blog' ),
+	path( 'blog/tags/<slug:tag_slug>/page/<int:page>',	views.BlogByTag.as_view(),	name = 'blog' ),
+	path( 'blog/<slug:slug>',							views.Post.as_view(),		name = 'post' ),
 ]
--- vazProjects/blogApp/views.py
+++ vazProjects/blogApp/views.py
@@ -7,6 +7,7 @@
 
 
 from django.views.generic import ListView, DetailView
+from django.urls import reverse
 
 from taggit.models import TaggedItem
 
@@ -31,9 +32,53 @@ class Blog( ListView ):
 	def get_context_data( self, **kwargs ):
 		context = super().get_context_data( **kwargs )
 		
-		context['tags'] = TaggedItem.tags_for( BlogPost, blogpost__draft = False )
+		# Tags in the sidebar.
+		context['allTags'] = TaggedItem.tags_for( BlogPost, blogpost__draft = False )
+		
+		# Pagination.
+		if context['page_obj'].has_next():
+			urlKwargs = { 'page': context['page_obj'].next_page_number(), **self.get_url_kwargs() }
+			
+			context['nextPageUrl'] = reverse( 'blogApp:blog', kwargs = urlKwargs )
+		
+		if context['page_obj'].has_previous():
+			urlKwargs = { 'page': context['page_obj'].previous_page_number(), **self.get_url_kwargs() }
+			
+			context['previousPageUrl'] = reverse( 'blogApp:blog', kwargs = urlKwargs )
 		
 		return context
+	
+	def get_url_kwargs( self ):
+		'''
+		Return a dictionary with keyword arguments to reverse the paginator urls.
+		'''
+		
+		return {}
+
+
+
+class BlogByTag( Blog ):
+	'''
+	Blog view filtered by tag.
+	'''
+	
+	allow_empty = False	# If we don't do this an invalid `tag_slug` will display an empty page.
+	
+	
+	def get_queryset( self ):
+		return super().get_queryset().filter( tags__slug = self.kwargs.get( 'tag_slug' ) )
+	
+	def get_url_kwargs( self ):
+		'''
+		Return a dictionary with keyword arguments to reverse the paginator urls.
+		'''
+		
+		urlKwargs = super().get_url_kwargs()
+		
+		urlKwargs['tag_slug'] = self.kwargs.get( 'tag_slug' )
+		
+		return urlKwargs
+
 
 
 class Post( DetailView ):
