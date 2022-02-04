f1b40e3853ae8223ca924388107eeb1ec21f8b69
Marcelo Tellier Sartori Vaz <marcelotsvaz@gmail.com>
2022-02-04 20:10:29 -0300

    Update AMI

--- doc/amiSetup.sh
+++ doc/amiSetup.sh
@@ -18,59 +18,58 @@ mountPoint=/mnt/new
 
 # Create Volume
 #---------------------------------------
-volumeID=$(aws ec2 create-volume --size 4 --availability-zone $availabilityZone --volume-type gp3 --no-encrypted --query 'VolumeId')
-aws ec2 wait volume-available --volume-ids $volumeID
-aws ec2 attach-volume --volume-id $volumeID --instance-id $instanceID --device /dev/sdf
-aws ec2 wait volume-in-use --volume-ids $volumeID
+volumeID=$(aws ec2 create-volume --size 2 --availability-zone ${availabilityZone} --volume-type gp3 --no-encrypted --query 'VolumeId')
+aws ec2 wait volume-available --volume-ids ${volumeID}
+aws ec2 attach-volume --volume-id ${volumeID} --instance-id ${instanceID} --device /dev/sdf
+aws ec2 wait volume-in-use --volume-ids ${volumeID}
 sleep 3
 export disk=$(lsblk -nro SERIAL,PATH | grep ${volumeID/-/} | cut -d ' ' -f2) # Get attached EBS volume device path.
 
 
 # Partitioning and Filesystem
 #---------------------------------------
-sgdisk --clear $disk
-sgdisk --new 1:0:+1M --change-name 1:'Boot' --typecode 1:ef02 $disk
-sgdisk --new 2:0:0 --change-name 2:'Root' $disk
+sgdisk --clear ${disk}
+sgdisk --new 1:0:+1M --change-name 1:'Boot' --typecode 1:ef02 ${disk}
+sgdisk --new 2:0:0 --change-name 2:'Root' ${disk}
 
 mkfs.ext4 ${disk}p2
 
-mkdir $mountPoint
-mount ${disk}p2 $mountPoint
+mkdir ${mountPoint}
+mount ${disk}p2 ${mountPoint}
 
 
 # Install Arch Linux
 #---------------------------------------
-pacstrap -c $mountPoint \
+pacstrap -c ${mountPoint} \
 base linux grub \
 openssh sudo aws-cli gdisk \
-man-db nano tmux mercurial \
-docker nginx memcached chromium dovecot postfix dehydrated \
-gcc libjpeg-turbo libmemcached
+nano \
+docker docker-compose nginx
 
-# Fstab
-genfstab -U $mountPoint >> $mountPoint/etc/fstab
+# Fstab.
+genfstab -U ${mountPoint} >> ${mountPoint}/etc/fstab
 
 
 # Chroot
 #---------------------------------------
-mount --bind /dev $mountPoint/dev
-mount --bind /sys $mountPoint/sys
-mount --bind /proc $mountPoint/proc
-chroot $mountPoint
+mount --bind /dev ${mountPoint}/dev
+mount --bind /sys ${mountPoint}/sys
+mount --bind /proc ${mountPoint}/proc
+chroot ${mountPoint}
 
 
 # Standard Configuration
 #---------------------------------------
-# Time zone
+# Time zone.
 ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
 
-# Localization
+# Localization.
 sed -Ei 's/^#(en_US\.UTF-8 UTF-8)/\1/g' /etc/locale.gen # Uncomment #en_US.UTF-8 UTF-8.
 sed -Ei 's/^#(pt_BR\.UTF-8 UTF-8)/\1/g' /etc/locale.gen # Uncomment #pt_BR.UTF-8 UTF-8.
 locale-gen
 echo 'LANG=en_US.UTF-8' > /etc/locale.conf
 
-# Network
+# Network.
 echo 'arch-ec2' > /etc/hostname
 
 #-------------------------------------------------------------------------------
@@ -85,8 +84,8 @@ EOF
 ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
 systemctl enable systemd-{networkd,resolved,timesyncd}
 
-# Bootloader
-grub-install $disk
+# Bootloader.
+grub-install ${disk}
 kernelParameters='nomodeset console=ttyS0,9600n8 earlyprintk=serial,ttyS0,9600 loglevel=6'
 sed -Ei "s/^(GRUB_CMDLINE_LINUX_DEFAULT)=.*/\1=\"${kernelParameters}\"/g" /etc/default/grub # Set GRUB_CMDLINE_LINUX_DEFAULT.
 sed -Ei 's/^(GRUB_TIMEOUT)=.*/\1=0/g' /etc/default/grub # Set GRUB_TIMEOUT=0.
@@ -95,7 +94,7 @@ grub-mkconfig -o /boot/grub/grub.cfg
 
 # Custom Configuration
 #---------------------------------------
-# Users
+# Users.
 mkdir -m 700 /etc/skel/.ssh
 touch /etc/skel/.ssh/authorized_keys
 rm /etc/skel/.bash*
@@ -103,13 +102,14 @@ rm /etc/skel/.bash*
 useradd -mG wheel marcelotsvaz
 
 
-# Sudo
+# Sudo.
 echo '%wheel ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/wheel
 
 
 # SSH
 #-------------------------------------------------------------------------------
 cat >> /etc/ssh/sshd_config << 'EOF'
+
 # Custom configuration.
 PasswordAuthentication no
 
@@ -126,6 +126,7 @@ EOF
 # Bash
 #-------------------------------------------------------------------------------
 cat >> /etc/bash.bashrc << 'EOF'
+
 # Custom configuration.
 export AWS_DEFAULT_REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/[a-z]$//')
 export AWS_DEFAULT_OUTPUT=table
@@ -133,7 +134,7 @@ export AWS_DEFAULT_OUTPUT=table
 green='\[\e[0;32m\]'
 blue='\[\e[1;34m\]'
 reset='\[\e[m\]'
-PS1="[\A][${green}\u@\h ${blue}\w${reset}]\$ "
+PS1="┌[\A][${green}\u@\h ${blue}\w${reset}]\$\n└> "
 
 alias grep='grep --color=auto'
 alias ls='ls --color=auto'
@@ -163,26 +164,26 @@ EOF
 #-------------------------------------------------------------------------------
 
 
-# Init scripts
+# Init scripts.
 mkdir /root/init
 
 #-------------------------------------------------------------------------------
 cat > /root/init/instanceScriptsSetup.sh << 'EOF'
 #!/bin/bash
-# Grow root partition
-partition=$(lsblk -nro MOUNTPOINT,PATH | grep '^/ ' | cut -d ' ' -f2)
-disk=${partition/p2/}
+# Grow root partition.
+disk="/dev/disk/by-uuid/$(lsblk -nro MOUNTPOINT,UUID | grep '^/ ' | cut -d ' ' -f2)"
+partition="/dev/disk/by-partuuid/$(lsblk -nro MOUNTPOINT,PARTUUID | grep '^/ ' | cut -d ' ' -f2)"
 
-sgdisk --move-second-header $disk
-sgdisk --delete 2 $disk
-sgdisk --new 2:0:0 --change-name 2:'Root' $disk
+sgdisk --move-second-header ${disk}
+sgdisk --delete 2 ${disk}
+sgdisk --new 2:0:0 --change-name 2:'Root' ${disk}
 
-partx -u $disk
+partx -u ${disk}
 
-resize2fs $partition
+resize2fs ${partition}
 
 
-# Download user data
+# Download user data.
 curl -s http://169.254.169.254/latest/user-data | tar -xz -C /root/init/
 EOF
 #-------------------------------------------------------------------------------
@@ -269,34 +270,34 @@ EOF
 systemctl enable instanceScriptsSetup perInstance perBoot perShutdown sshd docker
 
 
-# Clean up
+# Clean up.
 exit
-rm $mountPoint/{root/.bash_history,var/log/pacman.log}
+rm ${mountPoint}/{root/.bash_history,var/log/pacman.log}
 
 
 # Create AMI
 #---------------------------------------
-umount -R $mountPoint
-rm -r $mountPoint
-aws ec2 detach-volume --volume-id $volumeID
-aws ec2 wait volume-available --volume-ids $volumeID
+umount -R ${mountPoint}
+rm -r ${mountPoint}
+aws ec2 detach-volume --volume-id ${volumeID}
+aws ec2 wait volume-available --volume-ids ${volumeID}
 
 snapshotID=$(aws ec2 create-snapshot \
-    --volume-id $volumeID \
+    --volume-id ${volumeID} \
     --tag-specifications 'ResourceType=snapshot,Tags=[{Key=Name,Value=Arch Linux AMI Snapshot}]' \
     --query 'SnapshotId')
-aws ec2 wait snapshot-completed --snapshot-ids $snapshotID
-aws ec2 delete-volume --volume-id $volumeID
+aws ec2 wait snapshot-completed --snapshot-ids ${snapshotID}
+aws ec2 delete-volume --volume-id ${volumeID}
 
 imageId=$(aws ec2 describe-images --filters 'Name=name,Values=Arch Linux AMI' --query 'Images[0].ImageId')
-aws ec2 deregister-image --image-id $imageId
+aws ec2 deregister-image --image-id ${imageId}
 newImageId=$(aws ec2 register-image \
     --name 'Arch Linux AMI' \
     --architecture x86_64 \
     --virtualization-type hvm \
     --ena-support \
     --root-device-name /dev/xvda \
-    --block-device-mappings '[{"DeviceName": "/dev/xvda","Ebs":{"SnapshotId":"'$snapshotID'","VolumeType":"gp3"}}]' \
+    --block-device-mappings '[{"DeviceName": "/dev/xvda","Ebs":{"SnapshotId":"'${snapshotID}'","VolumeType":"gp3"}}]' \
     --query 'ImageId'
 )
 
