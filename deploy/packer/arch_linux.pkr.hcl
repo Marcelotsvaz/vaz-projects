6e06df4ab3fc0858fc95e40da2644e5f7b7a6f34
Marcelo Tellier Sartori Vaz <marcelotsvaz@gmail.com>
2022-08-30 02:01:46 -0300

    Make environment.env optional in user data

--- deploy/packer/arch_linux.pkr.hcl
+++ deploy/packer/arch_linux.pkr.hcl
@@ -20,7 +20,7 @@ locals {
 packer {
 	required_plugins {
 		amazon = {
-			version = ">= 1.1.3"
+			version = " >= 1.1.3"
 			source = "github.com/hashicorp/amazon"
 		}
 	}
@@ -41,8 +41,9 @@ source "amazon-ebssurrogate" "arch_linux" {
 	region = "sa-east-1"
 	subnet_id = "subnet-0171a8aa91d068ed7"
 	
+	# perInstance.sh file that just add the SSH key.
+	user_data = "H4sIAAAAAAAAA+3QwWqDQBAGYM99ii095FAaXe2u5FIISUisGKRpA/VSNrpo1GjY1UI89Nljekqh0FNoC/93mWVmYGdmL5VX6UZUsRzqzLgIq+cydorUZdZ5/GTfU8NyLcfpH9zq85Rxzg1iXWacr9p+eUWIsRMqlmXd6HfRfdf3U/2furk2W63MzbYyN0JnVzLOajLQOruTic0YHZFxb+IsOzGhZTT16PJ5xk45b+Gm813uiqeyyOvQ9wOHBbfJnKdBkU5H3iwq7Zf1nj++hpqsxxEJVZ3LuNFktVoQXx4G5IF8nJ/VHPb/mqJtslptO5m8FfKgf/tAAAAAAAAAAAAAAAAAAAAAAAAAf9QRk0aIXwAoAAA="
 	ebs_optimized = true
-	user_data_file = "ssh.tar.gz"
 	
 	ssh_agent_auth = true
 	ssh_username = "marcelotsvaz"
--- deploy/packer/scripts/chroot.sh
+++ deploy/packer/scripts/chroot.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/bash
 # 
 # VAZ Projects
 # 
@@ -164,26 +164,28 @@ EOF
 # Init scripts.
 #-------------------------------------------------------------------------------
 cat > /usr/local/lib/instanceScriptsSetup.sh << 'EOF'
-#!/bin/bash
+#!/usr/bin/bash
 # Grow root partition.
 disk="/dev/$(lsblk -nro MOUNTPOINT,PKNAME | grep '^/ ' | cut -d ' ' -f2)"
 partition="/dev/$(lsblk -nro MOUNTPOINT,KNAME | grep '^/ ' | cut -d ' ' -f2)"
 
+# Resize root partition.
 sgdisk --move-second-header ${disk}
 sgdisk --delete 2 ${disk}
 sgdisk --new 2:0:0 --change-name 2:'Root' ${disk}
 
+# Reload partitions.
 partx -u ${disk}
 
+# Resize filesystem.
 resize2fs ${partition}
 
 
 # Download user data.
 mkdir /tmp/deploy && cd ${_}
 curl -s http://169.254.169.254/latest/user-data | tar -xz
-# TODO: Make Optional.
 mv per*.sh /usr/local/lib/
-mv environment.env /etc/environment
+test -f environment.env && mv environment.env /etc/environment || true
 EOF
 #-------------------------------------------------------------------------------
 chmod +x /usr/local/lib/instanceScriptsSetup.sh
