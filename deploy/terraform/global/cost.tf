82097ba42cbb9dc3715ce024556e2ef6151cf1b3
Marcelo Tellier Sartori Vaz <marcelotsvaz@gmail.com>
2023-05-03 15:28:32 -0300

    Add Cost Anomaly Monitor

---
+++ deploy/terraform/global/cost.tf
@@ -0,0 +1,41 @@
+# 
+# VAZ Projects
+# 
+# 
+# Author: Marcelo Tellier Sartori Vaz <marcelotsvaz@gmail.com>
+
+
+
+resource aws_ce_anomaly_monitor main {
+	name = local.project_code
+	monitor_type = "DIMENSIONAL"
+	monitor_dimension = "SERVICE"
+	
+	tags = {
+		Name = "${local.project_name} Cost Anomaly Monitor"
+	}
+}
+
+
+resource aws_ce_anomaly_subscription test {
+	name = local.project_code
+	frequency = "DAILY"
+	monitor_arn_list = [ aws_ce_anomaly_monitor.main.arn ]
+	
+	subscriber {
+		type = "EMAIL"
+		address = "marcelotsvaz@gmail.com"
+	}
+	
+	threshold_expression {
+		dimension {
+			key = "ANOMALY_TOTAL_IMPACT_ABSOLUTE"
+			match_options = [ "GREATER_THAN_OR_EQUAL" ]
+			values = [ "10.0" ]
+		}
+	}
+	
+	tags = {
+		Name = "${local.project_name} Cost Anomaly Subscription"
+	}
+}
