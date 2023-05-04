c022b2b0b7a8fa2bdbe3b0719246778984d3a2f5
Marcelo Tellier Sartori Vaz <marcelotsvaz@gmail.com>
2023-05-04 20:24:35 -0300

    Refactor dehydrated script

--- loadBalancer/scripts/dehydrated.sh
+++ loadBalancer/scripts/dehydrated.sh
@@ -11,15 +11,14 @@
 set -e
 
 cd /home/${user}/deployment/tls/
-config=/home/${user}/loadBalancer/config/tls/dehydrated.conf
+config=../../loadBalancer/config/tls/dehydrated.conf
 
 tempFile=$(mktemp)
 # Run in a sub-shell because `set -e` doesn't trigger with `&&`.
-(dehydrated -f ${config} -p accountKey.pem -s websiteCsr.pem --accept-terms > ${tempFile} && cp ${tempFile} website.crt)
-(dehydrated -f ${config} -p accountKey.pem -s cloudfrontCsr.pem --accept-terms > ${tempFile} && cp ${tempFile} cloudfront.crt)
+(dehydrated --config ${config} --signcsr websiteCsr.pem > ${tempFile} && cp ${tempFile} website.crt)
+(dehydrated --config ${config} --signcsr cloudfrontCsr.pem > ${tempFile} && cp ${tempFile} cloudfront.crt)
 
-aws s3 cp website.crt s3://${bucket}/deployment/tls/ --no-progress
-aws s3 cp cloudfront.crt s3://${bucket}/deployment/tls/ --no-progress
+aws s3 sync . s3://${bucket}/deployment/tls/ --no-progress
 
-# Upload the CloudFront certificate to ACM, somehow.
+# Update the CloudFront certificate in ACM, somehow.
 cat cloudfront.crt | python -c 'import sys, re, subprocess; match = re.search( r"(-[\S\s]*?)\s\s(-[\S\s]*)", sys.stdin.read() ); subprocess.run( [ "aws" ] + match.expand( r"acm  --region  us-east-1  import-certificate  --certificate-arn  '${cloudfrontCertificateArn}'  --certificate  \1  --certificate-chain  \2  --private-key  file://cloudfrontKey.pem"  ).split("  ") )'
