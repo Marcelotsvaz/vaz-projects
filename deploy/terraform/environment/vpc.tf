862ac022ea77e569c5b69cc02f67d260b808a6cd
Marcelo Tellier Sartori Vaz <marcelotsvaz@gmail.com>
2023-12-04 21:30:18 -0300

    Fix invalid argument

--- deploy/terraform/environment/vpc.tf
+++ deploy/terraform/environment/vpc.tf
@@ -76,7 +76,16 @@ resource aws_security_group public {
 	
 	ingress {
 		description = "Traefik - HTTPS"
-		protocol = "all"	# UDP for HTTP/3.
+		protocol = "tcp"
+		from_port = 443
+		to_port = 443
+		cidr_blocks = [ "0.0.0.0/0" ]
+		ipv6_cidr_blocks = [ "::/0" ]
+	}
+	
+	ingress {
+		description = "Traefik - HTTPS (HTTP/3)"
+		protocol = "udp"
 		from_port = 443
 		to_port = 443
 		cidr_blocks = [ "0.0.0.0/0" ]
